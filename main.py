c7b5d2a8666fc14fc9775a07b8b4bae057101680
arshedgithub <arshedahmedmail@gmail.com>
2025-02-11 11:53:46 +0530

    testing command response

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 from deep_translator import GoogleTranslator
 from slack_sdk.errors import SlackApiError
-from flask import Flask, Response
+from flask import Flask, request, Response
 
 # Load environment variables
 env_path = Path('.') / '.env'
 load_dotenv(env_path)
 
 app = Flask(__name__)
-
-@app.route('/health', methods=['GET'])
-def health_check():
-    return ({"status": "healthy"})
 
 class SlackTranslateBot:
     def __init__(self):
@@ -34,12 +30,32 @@ class SlackTranslateBot:
         except Exception as e:
             print(f"Translation error: {e}")
 
+
+@app.route('/slack/translate', methods=['POST'])
+def translate_command():
+    data = request.form
+    text = data.get('text', '')
+    
+    try:
+        print(data)         
+        print("channel:", data['channel_id'])        
+        print(text)        
+        return Response(), 200
+    except Exception as e:
+        print(f"Error: {e}")
+        return Response(f"Error: {str(e)}"), 400  
+
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return ({"status": "healthy"})
+
+
 if __name__ == "__main__":
     app.run(debug=True) # automatically re run
     
-    bot = SlackTranslateBot()
-    text_to_translate="おはようございます, 今日の進捗はどうですか"
-    bot.handle_slash_command("test-bot", text_to_translate)
+    # bot = SlackTranslateBot()
+    # text_to_translate="おはようございます, 今日の進捗はどうですか"
         
         
     
